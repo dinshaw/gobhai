163ed9af665da78babc4cdf6f7b2c9b7d5a8e4a3
Dinshaw Gobhai <gobhai@gmail.com>
2012-12-24 22:25:58 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gobhai::Application.config.secret_token = '9e34518a52820dac9644928fd2da4ed5b3a0c0a0dadc59b8a1da017986d30837eeed1ec3a4db3c98b01ea8dce0d6c984afcc02e1805ca5eab7033918d38ab90b'
